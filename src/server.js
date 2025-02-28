0e6f19078ace74dc7bf533391afb91ae1351d330
Andrew Adams <andrewalufkin@gmail.com>
2025-02-28 02:55:38 -0600

    okay webhooks working now, changing port to fix EADDRINUSE

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const calculator = require('./calculator');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
